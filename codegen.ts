5304f1d87291eb39746dccf2033f27bc7bd1471a
Vitalii Kravchenko <vitalyx@gmail.com>
2023-04-02 18:40:12 +0100

    Add watch mode command

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:4000',
+  schema: 'http://localhost:4000', // URL of our GraphQL API
   // Generates types like `GetDogsQuery` and `GetDogsQueryVariables`.
   // Adding the 'typescript-react-apollo' plugin also makes it generate hooks like
   // `useGetDogsQuery` and document nodes like `GetDogsDocument`.
